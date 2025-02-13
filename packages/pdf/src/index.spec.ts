fdabad3c16b5a2033ac01beee7ad58c7518bc9f6
will griffin <willgriffin@gmail.com>
2025-02-13 09:16:08 -0700

    debug: increase logging on pdf failure

--- packages/pdf/src/index.spec.ts
+++ packages/pdf/src/index.spec.ts
@@ -1,5 +1,6 @@
 import { it, expect } from 'vitest';
-import path from 'path';
+import { join } from 'node:path';
+import { fileURLToPath } from 'node:url';
 import { extractTextFromPDF } from './index.js';
 // skipping because it takes too long and doesnt provide much value
 it.skip('should extract text from an image pdf', async () => {
@@ -11,11 +12,33 @@ it.skip('should extract text from an image pdf', async () => {
 
   // const text = await scribe.extractText([pdfPath]);
   // console.log(text);
-  const pdfPath = path.join(
-    __dirname,
+  const pdfPath = join(
+    fileURLToPath(new URL('.', import.meta.url)),
     '..',
-    '/Signed-Meeting-Minutes-October-8-2024-Regular-Council-Meeting-1.pdf',
+    'test',
+    'Signed-Meeting-Minutes-October-8-2024-Regular-Council-Meeting-1.pdf',
   );
   const text = await extractTextFromPDF(pdfPath);
+  expect(text).toBeDefined();
+  // console.log(text);
+}, 30000);
+
+it.skip('should extract text from a problematic pdf', async () => {
+  // const pdfPath = path.join(
+  //   __dirname,
+  //   '..',
+  //   '/Signed-Meeting-Minutes-October-8-2024-Regular-Council-Meeting-1.pdf',
+  // );
+
+  // const text = await scribe.extractText([pdfPath]);
+  // console.log(text);
+  const pdfPath = join(
+    fileURLToPath(new URL('.', import.meta.url)),
+    '..',
+    'test',
+    'Signed-Meeting-Minutes-October-8-2024-Regular-Council-Meeting-1.pdf',
+  );
+  const text = await extractTextFromPDF(pdfPath);
+  expect(text).toBeDefined();
   // console.log(text);
 }, 30000);
