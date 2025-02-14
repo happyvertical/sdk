38201be9a5940e39ab552e8f8a44025fba11b1eb
will griffin <willgriffin@gmail.com>
2025-02-14 08:19:19 -0700

    feat: export Card

--- packages/svelte/src/lib/index.ts
+++ packages/svelte/src/lib/index.ts
@@ -1,1 +1,16 @@
 // Reexport your entry components here
+import Root from './components/card/card.svelte';
+import Content from './components/card/card-content.svelte';
+import Header from './components/card/card-header.svelte';
+import Description from './components/card/card-description.svelte';
+import Footer from './components/card/card-footer.svelte';
+import Title from './components/card/card-title.svelte';
+
+export const Card = {
+	Root,
+	Content,
+	Header,
+	Description,
+	Footer,
+	Title
+} as const;
