edf178d36020bdd59481c2e3a954fdf78db0afbb
will griffin <willgriffin@gmail.com>
2025-02-15 05:29:39 -0700

    feat: package component exports

--- packages/svelte/src/lib/index.ts
+++ packages/svelte/src/lib/index.ts
@@ -1,22 +1,39 @@
-import Root from './components/card/card.svelte';
-import Content from './components/card/card-content.svelte';
-import Header from './components/card/card-header.svelte';
-import Description from './components/card/card-description.svelte';
-import Footer from './components/card/card-footer.svelte';
-import Title from './components/card/card-title.svelte';
+import CardRoot from './components/cards/Card.svelte';
+import Content from './components/cards/CardContent.svelte';
+import Header from './components/cards/CardHeader.svelte';
+import Description from './components/cards/CardDescription.svelte';
+import Footer from './components/cards/CardFooter.svelte';
+import Title from './components/cards/CardTitle.svelte';
 
+import ArticleRoot from './components/articles/Article.svelte';
+import ArticleListRoot from './components/articles/ArticleList.svelte';
+import ArticleListItem from './components/articles/ArticleListItem.svelte';
 export const Card: {
-	Root: typeof Root;
+	Root: typeof CardRoot;
 	Content: typeof Content;
 	Header: typeof Header;
 	Description: typeof Description;
 	Footer: typeof Footer;
 	Title: typeof Title;
 } = {
-	Root,
+	Root: CardRoot,
 	Content,
 	Header,
 	Description,
 	Footer,
 	Title
+};
+
+export const Article: {
+	Root: typeof CardRoot;
+} = {
+	Root: CardRoot
+};
+
+export const ArticleList: {
+	Root: typeof ArticleListRoot;
+	Item: typeof ArticleListItem;
+} = {
+	Root: ArticleListRoot,
+	Item: ArticleListItem
 };
