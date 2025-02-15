5a954768c8fefbc2183fca82827d12c4ac26dfd7
will griffin <willgriffin@gmail.com>
2025-02-15 08:41:46 -0700

    fix: removed build config

--- vitest.workspace.ts
+++ vitest.workspace.ts
@@ -4,6 +4,5 @@ export default defineWorkspace([
   './vitest.config.ts',
   './packages/smrt/vitest.config.ts',
   './packages/sql/vitest.config.ts',
-  './packages/svelte/vite.config.ts',
   './packages/pdf/vitest.config.ts',
 ]);
