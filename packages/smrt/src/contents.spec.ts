d74d1423b3fb789c429ddc24bfa1815578ee8a1e
will griffin <willgriffin@gmail.com>
2025-02-18 13:21:12 -0700

    fix: storybook fixes

--- packages/smrt/src/contents.spec.ts
+++ packages/smrt/src/contents.spec.ts
@@ -28,14 +28,10 @@ it.skip('should be able to getOrInsert a content item', async () => {
   };
 
   const content = await contents.getOrUpsert(fakeContentData);
-  // console.log({ content });
   expect(content.id).toBeDefined();
 
   const content2 = await contents.getOrUpsert(fakeContentData);
-  // console.log({ content2 });
   expect(content2.id).toBe(content.id);
-
-  // console.log({ content2 });
 });
 
 // skipped because it takes a long time
@@ -76,6 +72,7 @@ it.skip('should be able to sync a content dir', async () => {
   await contents.getOrUpsert({
     type: 'article',
     title: faker.lorem.sentence(),
+    description: faker.lorem.sentence(),
     body: faker.lorem.paragraph(),
     author: faker.person.fullName(),
     publish_date: faker.date.recent(),
--- packages/svelte/src/routes/+page.svelte
+++ packages/svelte/src/routes/+page.svelte
@@ -1,15 +1,10 @@
 <script>
-	import * as Card from '../lib/components/cards/index.js';
+	import ArticleList from '../lib/components/articles/ArticleList.svelte';
+	import ArticleListItem from '../lib/components/articles/ArticleListItem.svelte';
 </script>
 
-<Card.Root class="w-full max-w-sm">
-	<Card.Header>Hi</Card.Header>
-
-	<Card.Content>
-		<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</p>
-	</Card.Content>
-
-	<Card.Footer>
-		<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</p>
-	</Card.Footer>
-</Card.Root>
+<ArticleList>
+	<ArticleListItem title="Article 1" />
+	<ArticleListItem title="Article 2" />
+	<ArticleListItem title="Article 3" />
+</ArticleList>
